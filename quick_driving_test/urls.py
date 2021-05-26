ce62a3681af5cfb1c583ecd3e7bb36b90ad1ae3e
John <john@DESKTOP-HCRVJU7.localdomain>
2021-05-25 21:33:01 -0300

    added DRF

--- quick_driving_test/urls.py
+++ quick_driving_test/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
