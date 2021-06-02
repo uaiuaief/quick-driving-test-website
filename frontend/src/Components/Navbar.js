624a56619b930ceb46df2695180c77f137ec8cf2
John <john@DESKTOP-HCRVJU7.localdomain>
2021-06-02 19:09:05 -0300

    created css color scheme

---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Navbar extends Component {
+    render(){
+        return (
+            <>
+                {this.props.a}
+            </>
+
+        )
+    }
+}
+
+export {Navbar};
