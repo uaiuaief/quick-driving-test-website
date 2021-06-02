72a5e0d350bd2600e4b7d5dd8244ab14844a247c
John <john@DESKTOP-HCRVJU7.localdomain>
2021-06-02 20:45:42 -0300

    created components navbar and CTA and wrote basic css

---
+++ frontend/src/Components/CTA.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class CTA extends Component {
+    render() {
+        return (
+            <div id="CTA">
+                <div id="CTA-inner" className="inner-container">
+                    <h1>Find Quick Driving Test Cancellations</h1>
+                    <h2> We can get you a driving test cancellation booked at your preferred test centre for any date & times of your choosing. Let's get you an earlier practical driving test booked NOW</h2>
+                
+                    <div id="cards">
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export { CTA };
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,14 +1,25 @@
 import { Component } from "react";
 
 class Navbar extends Component {
-    render(){
+    render() {
         return (
-            <>
-                {this.props.a}
-            </>
+            <div id="navbar">
+                <div className="inner-container">
+                    <div id="logo-container">
+                        <img alt="logo"></img>
+                    </div>
+                    <ul id="navbar-buttons">
+                        <li><a href="#">How it Works</a></li>
+                        <li><a href="#">Pricing</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </div>
+            </div>
 
         )
     }
 }
 
-export {Navbar};
+export { Navbar };
