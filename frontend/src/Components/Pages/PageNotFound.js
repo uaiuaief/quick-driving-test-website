9d67416ee40b2785a2797a3b8c24e6ba760ef1e7
John <john@DESKTOP-HCRVJU7.localdomain>
2021-06-20 21:02:09 -0300

    finished 404 page

--- frontend/src/Components/Pages/PageNotFound.js
+++ frontend/src/Components/Pages/PageNotFound.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import notFound from '../../assets/not-found-icon.png'
 
 class PageNotFound extends Component {
     render() {
         return (
-            <div style={{height: "800px", paddingTop:"63px"}}>
-                <h1>404 Page not found</h1>
-            </div>
+            <section id="page-not-found">
+                <div className="inner-container">
+                    <img src={notFound}/>
+                    <h1>404</h1>
+                    <h2>Page not found</h2>
+                    <p>
+                    The page you are looking for doesn’t exist or an 
+                    other error occurred. Go back, or head over to 
+                    quickdrivintest.co.uk to choose a new direction. 
+                    </p>
+                </div>
+            </section>
         );
     }
 }
